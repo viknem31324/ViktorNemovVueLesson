ba2562e5988dcef498b3fe55f6142e7edc4c3e22
Виктор Немов <viktornemov2650@gmail.com>
2022-07-27 21:46:36 +0300

    Задание 2: добавил в data список, вывел список

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="wrapper">
     <h1 class="title">{{ message }}</h1>
+    <div class="list-group">
+      <AppTodoItem
+        v-for="task in todoItems"
+        v-bind:key="task.id"
+        v-bind:id="task.id"
+        v-bind:text="task.text"
+        v-bind:done="task.done"
+        @checkDone="checkDone"
+      ></AppTodoItem>
+    </div>
   </div>
 </template>
 
 <script>
+import todoItem from "./components/todoItem";
 export default {
   data() {
     return {
       message: "Hello from Vue App",
+      todoItems: [
+        { id: 1, text: "task 1", done: false },
+        { id: 2, text: "task 2", done: false },
+        { id: 3, text: "task 3", done: true },
+        { id: 4, text: "task 4", done: false },
+        { id: 5, text: "task 5", done: false },
+      ],
     };
+  },
+  components: {
+    AppTodoItem: todoItem,
+  },
+  methods: {
+    checkDone(arr) {
+      let a = this.todoItems.find((x) => x.id === arr[1]);
+      a.done = arr[0];
+    },
   },
 };
 </script>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="todoItem list-group-item">
+    <button
+      class="btn todoItem__btn"
+      v-bind:class="[val ? 'btn-success' : 'btn-secondary']"
+      v-on:click="checkDone"
+    >
+      {{ bool }}
+    </button>
+    <p class="todoItem__text">{{ text }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: Number,
+    text: String,
+    done: Boolean,
+  },
+  data() {
+    return {
+      val: this.done,
+    };
+  },
+  methods: {
+    checkDone() {
+      this.val = !this.val;
+      this.$emit("checkDone", [this.val, this.id]);
+    },
+  },
+  computed: {
+    bool() {
+      return !this.val ? "in order" : "complited";
+    },
+  },
+};
+</script>
+<style>
+.todoItem {
+  display: flex;
+}
+.todoItem__btn {
+  width: 100px;
+}
+.todoItem__text {
+  padding-left: 20px;
+}
+</style>
