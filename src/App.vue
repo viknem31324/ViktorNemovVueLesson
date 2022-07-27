08c6dcc95dbd64aacb25a6e88820e0840fafdc34
Виктор Немов <viktornemov2650@gmail.com>
2022-07-27 21:34:45 +0300

    Задание 1: подключил бутстрап, вывел поле message

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <h1 class="title">{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello from Vue App",
+    };
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  padding: 10px;
+}
+.title {
+  font-size: 20px;
+  text-align: center;
+}
+</style>
