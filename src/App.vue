e2e0e77ccbe207ccfeb110038fbbc3646122c8c8
Виктор Немов <viktornemov2650@gmail.com>
2022-08-10 23:01:10 +0300

    Задание 5: Навигационное меню

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="wrapper">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <router-link slot="li" class="nav-item btn btn-outline-secondary" exact to="/" active-class="btn-outline-success">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link slot="li" class="nav-item btn btn-outline-secondary" exact to="/about" active-class="btn-outline-success">
+            <a class="nav-link">About</a>
+          </router-link>
+          <router-link slot="li" class="nav-item btn btn-outline-secondary" exact to="/tasklist:user" active-class="btn-outline-success">
+            <a class="nav-link">tasklist</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -9,4 +24,14 @@ export default {};
 </script>
 
 <style>
+.collapse {
+  display: flex;
+  justify-content: center;
+}
+.nav-item {
+  width: 150px;
+}
+.nav-item:not(:last-child) {
+  margin-right: 40px;
+}
 </style>
